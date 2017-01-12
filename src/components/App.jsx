49ec6394b681369a0bbcc1fb6c31eec60a0c4627
Nathan Hatfield <nathan@HolyInfexBatman.attlocal.net>
2017-01-11 21:32:16 -0500

    Start working on switching about page to resume

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react';
 import { BrowserRouter, Match, Link } from 'react-router';
 import HomePage from './HomePage.jsx';
 import FuelSavingsPage from '../containers/FuelSavingsPage.jsx';
-import AboutPage from './AboutPage.jsx';
+import Resume from './resume/Resume.jsx';
 import NotFoundPage from './NotFoundPage.jsx';
 
 class App extends React.Component {
@@ -14,12 +14,12 @@ class App extends React.Component {
           {' | '}
           <Link to="/fuel-savings">Demo App</Link>
           {' | '}
-          <Link to="/about">About</Link>
+          <Link to="/resume">Résumé</Link>
           <br/>
           {this.children}
           <Match exactly pattern="/" component={HomePage}/>
           <Match exactly pattern="/fuel-savings" component={FuelSavingsPage} />
-          <Match exactly pattern="/about" component={AboutPage} />
+          <Match exactly pattern="/resume" component={Resume} />
           <Match exactly pattern="*" component={NotFoundPage} />
         </div>
       </BrowserRouter>
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router';
+
+import App from './components/App.jsx';
+import HomePage from './components/HomePage.jsx';
+import FuelSavingsPage from './containers/FuelSavingsPage.jsx'; // eslint-disable-line import/no-named-as-default
+import Resume from './components/resume/Resume.jsx';
+import NotFoundPage from './components/NotFoundPage.jsx';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={HomePage}/>
+    <Route path="fuel-savings" component={FuelSavingsPage}/>
+    <Route path="resume" component={Resume}/>
+    <Route path="*" component={NotFoundPage}/>
+  </Route>
+);
