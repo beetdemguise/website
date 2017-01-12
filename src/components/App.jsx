404bf08202b5239e6c638c2a4373074a448a17a4
Nathan Hatfield <nathan@HolyInfexBatman.attlocal.net>
2017-01-11 21:10:08 -0500

    Upgrade to react-router v4.0 and update code accordingly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,30 @@
 import React, { PropTypes } from 'react';
-import { Link, IndexLink } from 'react-router';
+import { BrowserRouter, Match, Link } from 'react-router';
+import HomePage from './HomePage.jsx';
+import FuelSavingsPage from '../containers/FuelSavingsPage.jsx';
+import AboutPage from './AboutPage.jsx';
+import NotFoundPage from './NotFoundPage.jsx';
 
-const App = (props) => {
-  return (
-    <div>
-      <IndexLink to="/">Home</IndexLink>
-      {' | '}
-      <Link to="/fuel-savings">Demo App</Link>
-      {' | '}
-      <Link to="/about">About</Link>
-      <br/>
-      {props.children}
-    </div>
-  );
+class App extends React.Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <div>
+          <Link to="/">Home</Link>
+          {' | '}
+          <Link to="/fuel-savings">Demo App</Link>
+          {' | '}
+          <Link to="/about">About</Link>
+          <br/>
+          {this.children}
+          <Match exactly pattern="/" component={HomePage}/>
+          <Match exactly pattern="/fuel-savings" component={FuelSavingsPage} />
+          <Match exactly pattern="/about" component={AboutPage} />
+          <Match exactly pattern="*" component={NotFoundPage} />
+        </div>
+      </BrowserRouter>
+    );
+  }
 };
 
 App.propTypes = {
